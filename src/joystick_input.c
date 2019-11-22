b241d76eb2c78028ff207eb25b2bd0fd96a81b35
Matt Spencer <matthew@thespencers.me.uk>
2019-11-22 17:20:31 +0000

    Add interrupt code for real joystick input

--- src/joystick_input.c
+++ src/joystick_input.c
@@ -32,44 +32,19 @@ typedef struct button {
 #define INPUT_DRV_NAME "GPIOC"
 
 struct button buttons[] = {
-  { pin: 4, key: INPUT_JOYSTICK_LEFT, name: "Left"  },
-  { pin: 2, key: INPUT_JOYSTICK_UP, name: "Up" },
-  { pin: 6, key: INPUT_JOYSTICK_DOWN, name: "Down" },
-  { pin: 8, key: INPUT_JOYSTICK_RIGHT, name: "Right" },
-  { pin: 3, key: INPUT_JOYSTICK_A, name: "A" },
-  { pin: 5, key: INPUT_JOYSTICK_B, name: "B" },
-  { pin: 7, key: INPUT_JOYSTICK_C, name: "C" },
-  { pin: 9, key: INPUT_JOYSTICK_D, name: "D" },
+  { pin: 4, key: INPUT_JOYSTICK_LEFT,   name: "Left"  },
+  { pin: 2, key: INPUT_JOYSTICK_UP,     name: "Up" },
+  { pin: 6, key: INPUT_JOYSTICK_DOWN,   name: "Down" },
+  { pin: 8, key: INPUT_JOYSTICK_RIGHT,  name: "Right" },
+  { pin: 3, key: INPUT_JOYSTICK_A,      name: "A" },
+  { pin: 5, key: INPUT_JOYSTICK_B,      name: "B" },
+  { pin: 7, key: INPUT_JOYSTICK_C,      name: "C" },
+  { pin: 9, key: INPUT_JOYSTICK_D,      name: "D" },
 };
-static int numButtons = sizeof(buttons) / sizeof(button);
-
-struct device *inputDev;
-
-// id ignored for now - I only have one joystick anyway!
-void joystick_input_read(int id, joystickState *state) {
-  state->x=0;
-  state->y=0;
-  for(int i=0; i<numButtons; i++) {
-    button *b = &buttons[i];
-    if(true) {//pins & BIT(b->pin)) {
-      u32_t val;
-      gpio_pin_read(inputDev, b->pin, &val);
-      switch(b->key) {
-        case INPUT_JOYSTICK_A: state->b1 = val==0 ? 1 : 0; break;
-        case INPUT_JOYSTICK_B: state->b2 = val==0 ? 1 : 0; break;
-        case INPUT_JOYSTICK_C: state->b3 = val==0 ? 1 : 0; break;
-        case INPUT_JOYSTICK_D: state->b4 = val==0 ? 1 : 0; break;
-        case INPUT_JOYSTICK_UP: if(!val) state->y=127; break;
-        case INPUT_JOYSTICK_DOWN: if(!val) state->y=-127; break;
-        case INPUT_JOYSTICK_LEFT: if(!val) state->x=-127; break;
-        case INPUT_JOYSTICK_RIGHT: if(!val) state->x=127; break;
-      }
-    //  LOG_INF("Button %d %s => %d", i, b->name, val);
-    }
-  }
-}
+static int numButtons = ARRAY_SIZE(buttons);
 
 #if USE_DEBUG_JOYSTICK
+
 static int debugJoystickId=0;
 static void joystick_debug_update_state(joystickState *state) {
   static s8_t x=0, y=0;
@@ -90,29 +65,92 @@ static void joystick_debug_emit(struct k_timer *timer) {
 }
 
 K_TIMER_DEFINE(joystick_debug_timer, joystick_debug_emit, NULL);
-#endif
 
 void joystick_input_init() {
-#if USE_DEBUG_JOYSTICK
   k_timer_start(&joystick_debug_timer, K_MSEC(40), K_MSEC(40));
+}
+
 #else
-  int ret;
-  inputDev = device_get_binding(INPUT_DRV_NAME);
+
+static struct gpio_callback gpio_cb;
+
+static void setJoystickState(joystickState *state, int key, int val) {
+  switch(key) {
+    case INPUT_JOYSTICK_A: state->b1 = val; break;
+    case INPUT_JOYSTICK_B: state->b2 = val; break;
+    case INPUT_JOYSTICK_C: state->b3 = val; break;
+    case INPUT_JOYSTICK_D: state->b4 = val; break;
+    case INPUT_JOYSTICK_UP: if(!val) state->y=127; break;
+    case INPUT_JOYSTICK_DOWN: if(!val) state->y=-127; break;
+    case INPUT_JOYSTICK_LEFT: if(!val) state->x=-127; break;
+    case INPUT_JOYSTICK_RIGHT: if(!val) state->x=127; break;
+  }
+}
+
+void button_pressed(struct device *dev, struct gpio_callback *cb, u32_t pins) {
+  u32_t val=0;
+  int err, index=0;
+  int id=0;
+
+  joystickState *state=joystick_hid_get_state(id);
+
+  // Walk over all the configured inputs to see which one changed
+  for(; index<numButtons; index++) {
+    struct button *b=&buttons[index];
+    if(pins & BIT(b->pin)) {
+      err = gpio_pin_read(dev, b->pin, &val);
+      if(err!=0) {
+        LOG_ERR("Error reading pin");
+      }
+      printk("Button %s => %d\n", b->name, val);
+      setJoystickState(state, b->key, val);
+    }
+  }
+  joystick_post_update(id, state);
+}
+
+void joystick_bootstrap_state(struct device *dev, int id) {
+  joystickState *state=joystick_hid_get_state(id);
+  u32_t val;
+  int err, index=0;
+  memset(state, 0, sizeof(joystickState));
+  state->x=0; state->y=0;
+  for(; index<numButtons; index++) {
+    struct button *b=&buttons[index];
+    err = gpio_pin_read(dev, b->pin, &val);
+    setJoystickState(state, b->key, val);
+  }
+  joystick_post_update(id, state);
+}
+
+
+void joystick_input_init() {
+  int ret, index=0;
+  u32_t mask = 0;
+  static u32_t flags = (GPIO_DIR_IN | GPIO_INT | GPIO_PUD_PULL_DOWN |
+    GPIO_INT_EDGE | GPIO_INT_DOUBLE_EDGE | GPIO_INT_DEBOUNCE);
+
+  struct device *inputDev = device_get_binding(INPUT_DRV_NAME);
   if(!inputDev) {
     LOG_ERR("Cannot find input device %s", INPUT_DRV_NAME);
     return;
   }
 
-  // Configure the input pins as inpts
-  u32_t flags = GPIO_DIR_IN | GPIO_PUD_PULL_UP | GPIO_INT_DEBOUNCE;
-
-  for(int i=0; i<numButtons; i++) {
-    button *b = &buttons[i];
-    ret = gpio_pin_configure(inputDev, b->pin,flags);
+  for(index=0; index<numButtons; index++) {
+    button *b = &buttons[index];
+    ret = gpio_pin_configure(inputDev, b->pin, flags);
     if(ret) {
       LOG_ERR("Error configuring %s pin %d", INPUT_DRV_NAME, b->pin);
       return;
     }
+    mask |= BIT(b->pin);
+    gpio_pin_enable_callback(inputDev, b->pin);
   }
-#endif
+
+  gpio_init_callback(&gpio_cb, button_pressed, mask);
+
+  gpio_add_callback(inputDev, &gpio_cb);
+
+  joystick_bootstrap_state(inputDev, 0);
 }
+#endif
--- src/joystick_input.h
+++ src/joystick_input.h
@@ -4,6 +4,5 @@
 #include "joystick_hid.h"
 
 void joystick_input_init();
-void joystick_input_read(int id, joystickState *state);
 
 #endif
